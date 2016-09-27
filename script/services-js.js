b8b399d069b463f2dbd58fd920065f28afa0d4cd
Ginger Blumenthal <ginbutterfly@gmail.com>
2016-09-26 22:15:53 -0400

    more updates to services page

--- script/services-js.js
+++ script/services-js.js
@@ -1,4 +1,36 @@
+
+$("button").hide();
+
 $(document).ready(function(){
-	
-	
+
+	$("#treatment1").click(function(){
+        $(".dropbtn1").slideToggle("slow");
+    });
+
+  $("#treatment2").click(function(){
+        $(".dropbtn2").slideToggle("slow");
+    });
+
 });
+
+
+	
+ 
+// $(document).ready(function(){
+
+// $(window).scroll(function() {
+//     if ($(this).scrollTop() > 0) {
+//         // apply effects and animations
+   
+
+// 	$(".dropbtn").animate({
+// 		right: '250px',
+//       height: '+=100px',
+//       width: '+=100px'
+
+//     });
+	
+// 	 }
+// });
+	
+// });
